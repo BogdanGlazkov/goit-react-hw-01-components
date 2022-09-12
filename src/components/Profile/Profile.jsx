f01374f16b0cd03eb07a3459f22b1a5953369eba
BogdanGlazkov <bglazkov@i.ua>
2022-09-12 22:28:28 +0300

    Add 2 tasks

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,38 @@
+import PropTypes from 'prop-types';
+import s from './Profile.module.css';
+
+const Profile = ({
+  username,
+  tag,
+  location,
+  avatar,
+  stats: { followers, views, likes },
+}) => {
+  return (
+    <div className={s.profile}>
+      <div className={s.description}>
+        <img className={s.avatar} src={avatar} alt={username} />
+        <p className={s.name}>{username}</p>
+        <p className={s.tag}>@{tag}</p>
+        <p className={s.location}>{location}</p>
+      </div>
+
+      <ul className={s.stats}>
+        <li className={s.item}>
+          <span className={s.label}>Followers</span>
+          <span className={s.quantity}>{followers}</span>
+        </li>
+        <li className={s.item}>
+          <span className={s.label}>Views</span>
+          <span className={s.quantity}>{views}</span>
+        </li>
+        <li className={s.item}>
+          <span className={s.label}>Likes</span>
+          <span className={s.quantity}>{likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Profile;
